d92c25535341685f563f679975aae51dfe5910f8
L-V-Dragon <viotti37@gmail.com>
2022-11-17 17:48:44 +0100

    Deciso di togliere il bonus

--- script.js
+++ script.js
@@ -59,17 +59,3 @@ function stampaPersona(membro) {
     `
     )
 }
-
-
-
-
-
-/*for (let persona of membro){
-    document.getElementById("cardMembro").innerHTML +=
-    `<div class="col-12 col-lg-3 col-md-5 card mx-4 my-4 px-0">
-    <img id="foto" scr=".img/${persona.foto}" class="card-img-top" alt="">
-    <div id="nome" class="card-body fw-bold pt-4 pb-0 fs-5 text-muted">${persona.nome}</div>
-    <div id="ruolo" class="card-body fw-lighter pt-0 pb-4 fs-6 text-muted">${persona.ruolo}</div>
-    </div>
-    `
-}*/
